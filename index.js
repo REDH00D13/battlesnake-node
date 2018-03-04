cc50f362ceadd4d0da943a8d244f7be0316e742c
saffron <Nightwingkid310@gmail.com>
2018-03-03 16:03:02 -0800

    snaky wins!!!

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.post('/move', (request, response) => {
 const coffee = request.body
 const y = coffee.you.body.data[0].y;
 const height = coffee.height;
-
+const width = coffee.width;
  let direction = 'up'
 if (y === 0) direction = 'right'
-
+if (x ===width-1) direction = 'down'
 
   // Response data
   const data = {
